9cad9e298aa0d1b9bb7f5183de3da6aa4726a90e
mhwalid7 <mhwalid7@gmail.Com>
2023-07-10 14:09:13 +0200

    [Authentification][Form] Ajout des formulaires de connexion et d'inscription, sans utilisation de l'Api back-end

---
+++ src/components/Register.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    title="S'inscrire"
+  >
+    <v-container>
+      <v-text-field
+        v-model="first"
+        color="primary"
+        label="Prénom"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="last"
+        color="primary"
+        label="Nom"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="last"
+        color="primary"
+        label="Date de naissance"
+        variant="underlined"
+        type="date"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        color="primary"
+        label="Email"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password"
+        color="primary"
+        label="Mots de passe"
+        placeholder="Enter your password"
+        type="password"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="terms"
+        color="secondary"
+        label="J'accepter les conditions du site"
+      ></v-checkbox>
+    </v-container>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+
+      <v-btn color="success">
+        S'enregistrer
+
+        <v-icon icon="mdi-chevron-right" end></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      first: null,
+      last: null,
+      email: null,
+      password: null,
+      terms: false,
+    }),
+  }
+</script>
